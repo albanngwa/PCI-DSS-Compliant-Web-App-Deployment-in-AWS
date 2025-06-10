c829f7be91528450621057b05e754556439b359c
Alban Ngwa <Ngwaambe51@gmail.com>
2025-06-09 19:33:01 -0500

    completed vpc

---
+++ PCI_DSS/backend.tf
@@ -0,0 +1,9 @@
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket    = "ngwa-terraform-remote-states"
+    key       = "pci_dss_state"
+    region    = "us-east-2"
+    profile   = "alban"
+  }
+}
